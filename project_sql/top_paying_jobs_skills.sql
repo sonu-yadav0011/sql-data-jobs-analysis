494ff4d2c5e0b37462451a9dc89d339bf721c88c
Sonu Yadav <so.yadav0011@gmail.com>
2025-12-04 19:19:47 +0530

    completed all the code

---
+++ project_sql/top_paying_jobs_skills.sql
@@ -0,0 +1,27 @@
+WITH top_jobs AS (
+    SELECT 
+        job.job_id,
+        job.job_title,
+        company_dim.name AS company_name,
+        job.salary_year_avg
+    FROM job_postings_fact AS job
+    LEFT JOIN company_dim 
+        ON job.company_id = company_dim.company_id 
+    WHERE 
+        job_title_short = 'Data Analyst'
+        AND job_location = 'Anywhere'
+        AND salary_year_avg IS NOT NULL
+    ORDER BY salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_jobs.*,
+    skills_dim.skills
+FROM top_jobs
+INNER JOIN skills_job_dim
+    ON top_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC;
